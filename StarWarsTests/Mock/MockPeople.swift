7c154ab36d4227453d7aa69938788977f6dead93
Philipp Tschauner <munichboi@gmail.com>
2024-04-14 20:24:03 +0200

    multiple lines

--- StarWarsTests/Mock/MockPeople.swift
+++ StarWarsTests/Mock/MockPeople.swift
@@ -11,9 +11,42 @@ import Foundation
 extension People {
 
     static let mockCharacters = [
-        Character(birthYear: "19BBY", eyeColor: "blue", films: ["A New Hope", "The Empire Strikes Back", "Return of the Jedi"], gender: "male", hairColor: "blond", name: "Luke Skywalker"),
-        Character(birthYear: "112BBY", eyeColor: "yellow", films: ["A New Hope", "The Empire Strikes Back", "Return of the Jedi"], gender: "n/a", hairColor: "n/a", name: "C-3PO"),
-        Character(birthYear: "41.9BBY", eyeColor: "yellow", films: ["The Phantom Menace", "Attack of the Clones", "Revenge of the Sith"], gender: "male", hairColor: "none", name: "Darth Vader")
+        Character(
+            birthYear: "19BBY",
+            eyeColor: "blue",
+            films: [
+                "A New Hope",
+                "The Empire Strikes Back",
+                "Return of the Jedi"
+            ],
+            gender: "male",
+            hairColor: "blond",
+            name: "Luke Skywalker"
+        ),
+        Character(
+            birthYear: "112BBY",
+            eyeColor: "yellow",
+            films: [
+                "A New Hope",
+                "The Empire Strikes Back",
+                "Return of the Jedi"
+            ],
+            gender: "n/a",
+            hairColor: "n/a",
+            name: "C-3PO"
+        ),
+        Character(
+            birthYear: "41.9BBY",
+            eyeColor: "yellow",
+            films: [
+                "The Phantom Menace",
+                "Attack of the Clones",
+                "Revenge of the Sith"
+            ],
+            gender: "male",
+            hairColor: "none",
+            name: "Darth Vader"
+        )
     ]
 
     static let mockPeople = People(result: mockCharacters)
