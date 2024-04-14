580c7a4f09153233c2ee2b507b70f61eff1bc54c
Philipp Tschauner <munichboi@gmail.com>
2024-04-14 17:39:05 +0200

    Initial Commit

---
+++ StarWars/StarWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsApp.swift
+//  StarWars
+//
+//  Created by Philipp Tschauner on 14.04.24.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
